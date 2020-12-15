00db06b27737f53e1d515c30715c90718dbce265
wyz <1076499338@qq.com>
2020-12-15 22:04:17 +0800

    update

--- src/Displayer.cpp
+++ src/Displayer.cpp
@@ -13,11 +13,14 @@ void Displayer::render()
     SDL_Event event;
     bool exit=false;
     SDL_Rect rect{0,0,1200,900};
-    SDL_Surface* surface=SDL_LoadBMP("background.bmp");
+    SDL_Surface* surface1=SDL_LoadBMP("background.bmp");
+    SDL_Surface* surface2=SDL_LoadBMP("image.bmp");
     SDL_CHECK
-    SDL_Texture* texture=SDL_CreateTextureFromSurface(renderer,surface);
+    SDL_Texture* texture1=SDL_CreateTextureFromSurface(renderer,surface1);
+    SDL_Texture* texture2=SDL_CreateTextureFromSurface(renderer,surface2);
     SDL_CHECK
-    while(!exit){
+    bool t=true;
+    auto processEvent=[&exit,&event,&t](){
         while(SDL_PollEvent(&event)){
             switch (event.type) {
                 case SDL_QUIT: {
@@ -28,12 +31,32 @@ void Displayer::render()
                         case SDLK_ESCAPE:{
                             exit=true;
                         }break;
+                        case SDLK_n:{
+                            t=!t;
+                            std::cout<<t<<std::endl;
+                        }break;
                     }
                 } break;
+                case SDL_MOUSEWHEEL:{
+
+                }break;
+                case SDL_MOUSEMOTION:{
+
+                }break;
             }
         }
+    };
+
+    /**
+     * render loop
+     */
+    while(!exit){
+        processEvent();
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer,texture, nullptr,&rect);
+        if(t)
+            SDL_RenderCopy(renderer,texture1, nullptr,&rect);
+        else
+            SDL_RenderCopy(renderer,texture2, nullptr,&rect);
         SDL_RenderPresent(renderer);
     }
 
