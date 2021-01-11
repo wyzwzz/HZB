a05d6ef370f0e488508987f44c076c002e0ebe8c
wyz <1076499338@qq.com>
2021-01-11 20:08:26 +0800

    update

--- src/RenderOBJ.cpp
+++ src/RenderOBJ.cpp
@@ -17,7 +17,7 @@ RenderOBJ::RenderOBJ(std::string obj_file_path)
 {
     START_TIMER
     tinyobj::ObjReaderConfig reader_config;
-    reader_config.mtl_search_path = "./nanosuit";
+//    reader_config.mtl_search_path = "./nanosuit";
 
     tinyobj::ObjReader reader;
 
